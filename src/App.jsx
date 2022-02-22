e60c4f274ad6b8acc5638a4a7afa359207fe700e
himchan94 <himchan9494@gmail.com>
2022-02-22 13:42:33 +0900

    feat: Applied MAP API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import GlobalStyle from "./GlobalStyle";
+import { NativeBar } from "./component";
+import { MapPage } from "./page/index";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <NativeBar />
+      <MapPage />
+    </>
+  );
 };
 
 export default App;

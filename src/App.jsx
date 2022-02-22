a7ee82ea571323ddc0967b347312309982dea44f
auhy49 <auhy49@gmail.com>
2022-02-23 02:01:16 +0900

    bug fixed develop branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
-import { NativeBar,NativeFooter } from "./component";
+import { NativeBar, NativeFooter } from "./component";
 import { MapPage, Search, Info, Splash } from "./page/index";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
@@ -17,8 +17,8 @@ const App = () => {
 					<Route path="search" element={<Search />} />
 				</Routes>
 			</Router>
-      <NativeFooter />
+			<NativeFooter />
 		</>
 	);
-
+};
 export default App;
